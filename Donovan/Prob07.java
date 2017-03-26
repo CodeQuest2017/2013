3a20b1019a6db6313c939c0bcc432814d4be8895
Donovan Glover <UltimateDonovan@gmail.com>
2017-03-26 15:24:53 -0400

    Fix solution for #7 by using ints instead of doubles :+1:

--- Donovan/Prob07.java
+++ Donovan/Prob07.java
@@ -1,4 +1,3 @@
-// INCOMPLETE
 import java.util.*;
 import java.io.*;
 
@@ -7,25 +6,29 @@ public class Prob07 {
 	public static Map<String, Integer> map = new LinkedHashMap<String, Integer>();
 	public static final String[] name = new String[]
 		{"PENNY", "NICKEL", "DIME", "QUARTER", "ONE", "FIVE", "TEN", "TWENTY"};
-	public static final double[] value = new double[]
-		{0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20};
+	public static final int[] value = new int[]
+		{1, 5, 10, 25, 100, 500, 1000, 2000};
 	public static void main(String[] args) throws IOException {
 		Scanner scan = new Scanner(new File(dir + "Prob07.in.txt"));
 		while(scan.hasNextLine()) {
 			String[] in = scan.nextLine().split(", ");
-			double left = Double.parseDouble(in[1]) - Double.parseDouble(in[0]);
+			int left = (int) (Double.parseDouble(in[1]) * 100 - Double.parseDouble(in[0]) * 100);
+			String r = String.valueOf(left / 100.0);
+			if(r.charAt(0) == '0') r = r.substring(1);
+			int index = r.indexOf(".");
+			if(r.length() == index + 2) r = r + "0";
+			System.out.print("$" + r);
 			if(left == 0) {
 				System.out.println("NONE");
 				System.exit(0);
 			}
-			System.out.print(left);
-			while(left >= 0.01) left = next(left);
+			while(left >= 1) left = next(left);
 			map.forEach((k, v) -> System.out.print(", " + k + "=" + v));
 			System.out.println();
 			map.clear();
 		}
 	}
-	public static double next(double n) {
+	public static int next(int n) {
 		int c = -1;
 		for(int i = 0, x = value.length; i < x; i++) {
 			if(n >= value[i]) c++;
