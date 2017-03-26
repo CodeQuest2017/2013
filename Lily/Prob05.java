cf921b823c8b1689a68bd38aa6f1c4a990286a0a
Donovan Glover <UltimateDonovan@gmail.com>
2017-03-26 18:05:19 -0400

    Naming conventions :fallen_leaf:

---
+++ Lily/Prob05.java
@@ -0,0 +1,43 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Prob05{
+    public static void main(String[] args)throws Exception{
+        File file=new File("Prob05.in.txt");
+        Scanner scan=new Scanner(file);
+        String input = "";
+        
+        String[] present = scan.nextLine().split("/");
+        String[] newDate;
+        String soft;
+        int days = 0;
+        int[] monthDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+        
+        while (scan.hasNextLine()){
+            days = 0;
+            input=""+scan.nextLine();
+            newDate = input.split(":")[1].split("/");
+            soft = input.split(":")[0];
+            
+            while(!Arrays.toString(present).equals(Arrays.toString(newDate))){
+                if (Integer.parseInt(newDate[1])>=monthDays[Integer.parseInt(newDate[0])-1]){
+                    newDate[1]="1";
+                    if(Integer.parseInt(newDate[0])>11){
+                        newDate[0] = "1";
+                        newDate[2] = ""+(Integer.parseInt(newDate[2])+1);
+                    }
+                    else newDate[0] = ""+(Integer.parseInt(newDate[0])+1);
+                }
+                else newDate[1]=""+(Integer.parseInt(newDate[1])+1);
+                days++;
+            }
+            
+            if (days>=180){
+                System.out.println(soft+", "+days);
+            }
+        }
+        
+        scan.close();
+    }
+}
